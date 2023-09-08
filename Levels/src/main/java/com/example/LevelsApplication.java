7b7d70b26da9a57898cdcb00ef610deeda096220
riku14 <115448381+riku14@users.noreply.github.com>
2023-09-08 19:23:59 +0900

    test

---
+++ Levels/src/main/java/com/example/LevelsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelsApplication.class, args);
+	}
+
+}
---
+++ Levels/src/main/java/com/example/repository/UserMapper.java
@@ -0,0 +1,18 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.domain.user.model.MUser;
+
+@Mapper
+public interface UserMapper {
+	
+	//ユーザー登録
+	public int insertOne(MUser user);
+	
+	//ユーザー複数件取得
+	public List<MUser> findMany();
+
+}
